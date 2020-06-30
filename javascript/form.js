b2d10d299039d5fb8914db6d1736db10d4ec8393
hckim96 <hyeoncheolkim96@gmail.com>
2020-06-30 16:43:43 +0900

    form, button, submit click event handling

---
+++ javascript/form.js
@@ -0,0 +1,51 @@
+const formInput = document.querySelector('.form-input'),
+    formText = document.querySelector('.form-text'),
+    formDelete = document.querySelector('.form-delete');
+
+const USER_NAME_KEY = 'userName';
+
+function saveUserName(userName) {
+    localStorage.setItem(USER_NAME_KEY, userName);
+}
+function getUserName() {
+    formInput.classList.remove('hide');
+    formDelete.classList.add('hide');
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.querySelector('.form-input').value);
+    saveUserName(formInput.value);
+    greeting(formInput.value);
+    formInput.value = '';
+}
+function handleDelClick(e) {
+    localStorage.removeItem(USER_NAME_KEY);
+    formText.innerText = '';
+
+    loadUserName();
+}
+document.querySelector('form').addEventListener('submit', handleSubmit);
+formDelete.addEventListener('click', handleDelClick);
+
+function greeting(userName) {
+    formInput.classList.add('hide');
+    formDelete.classList.remove('hide');
+    formText.innerText = `hello, ${userName}`;
+}
+function loadUserName() {
+    const currentUserName = localStorage.getItem(USER_NAME_KEY);
+    if (
+        currentUserName &&
+        currentUserName !== undefined &&
+        currentUserName !== null
+    ) {
+        //thereis user
+
+        greeting(currentUserName);
+    } else {
+        getUserName();
+    }
+}
+
+loadUserName();
