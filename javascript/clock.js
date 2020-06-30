d63893473053e4038d0cf23d87a111f2d85b3c93
hckim96 <hyeoncheolkim96@gmail.com>
2020-06-30 12:00:52 +0900

    jsNC24

---
+++ javascript/clock.js
@@ -0,0 +1,20 @@
+// setinterval
+
+const clock = document.querySelector('.clock');
+
+clock.innerHTML = '123456';
+
+function getTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    clock.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+getTime();
+
+setInterval(getTime, 1000);
---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// queryselector, eventlistener, classList (toggle: add, remove)
+
+console.log(document.querySelector('.h1class'));
+console.log(document.querySelector('#h1id'));
+
+const headerTitle = document.querySelector('.h1class');
+
+function handleClick(e) {
+    console.log(`e : ${e}`);
+    console.log(e.target);
+    console.log('headertitleclicked');
+    if (e.target.style.color === 'black') {
+        e.target.style.color = 'red';
+    } else {
+        e.target.style.color = 'black';
+    }
+}
+headerTitle.addEventListener('click', handleClick);
