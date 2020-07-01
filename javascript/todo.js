90230abafe8e2be5d8f239d791c7f3aad3a8640a
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-01 12:00:42 +0900

    implementing del btn

--- javascript/todo.js
+++ javascript/todo.js
@@ -3,17 +3,48 @@ const todoForm = document.querySelector('.todo-form'),
     todoList = document.querySelector('.todo-list');
 
 const TODO_LS = 'todo';
-
-function addTodo(text) {
+let todos = [];
+function handleDeleteButtonClick(e) {
+    e.preventDefault();
+    const button = e.target;
+    const li = button.parentNode;
+    console.log(li);
+}
+function paintTodo(text) {
     const li = document.createElement('li');
+    li.id = todos.length === 0 ? 0 : todos[todos.length - 1].id + 1;
+
     const span = document.createElement('span');
     span.innerText = text;
 
     const deleteButton = document.createElement('button');
     deleteButton.innerText = '❌';
+    deleteButton.addEventListener('click', handleDeleteButtonClick);
     li.appendChild(span);
     li.appendChild(deleteButton);
     todoList.appendChild(li);
+}
+function loadTodos() {
+    todos = JSON.parse(localStorage.getItem(TODO_LS));
+    todos.forEach(function (todo) {
+        addTodo(todo.text);
+    });
+}
+
+function saveTodos() {
+    localStorage.setItem(TODO_LS, JSON.stringify(todos));
+}
+
+function addTodo(text) {
+    paintTodo(text);
+
+    const todo = {
+        text: text,
+        id: todos.length === 0 ? 0 : todos[todos.length - 1].id + 1,
+    };
+    todos.push(todo);
+    console.log(todos);
+    saveTodos();
 }
 function handleSubmit(e) {
     e.preventDefault();
@@ -23,3 +54,4 @@ function handleSubmit(e) {
 }
 
 todoForm.addEventListener('submit', handleSubmit);
+loadTodos();
