ad821bbb9da4a5632c128e3904a8ea426651b52f
hyeoncheolkim <hyeoncheolkim96@gmail.com>
2020-07-02 23:26:00 +0900

    implement weather

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,11 @@
-
-
-const headerTitle = document.querySelector('.h1class');
+const headerTitle = document.querySelector('.clock');
 
 function handleClick(e) {
-    console.log(`e : ${e}`);
-    console.log(e.target);
-    console.log('headertitleclicked');
     if (e.target.style.color === 'black') {
         e.target.style.color = 'red';
     } else {
         e.target.style.color = 'black';
     }
 }
+
 headerTitle.addEventListener('click', handleClick);
--- javascript/weather.js
+++ javascript/weather.js
@@ -1,49 +1,44 @@
 const API_KEY = 'f45610611a10099a1ac47767f60fb43c';
 
+const COORDS = 'coords';
 
-
-const COORDS = "coords";
-
+const weather = document.querySelector('.weather');
 
 function getWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    .then(function (response) {
-        console.log(response);
-        const weatherObj = response.json();
-        return weatherObj;
-    })
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+        .then(function (response) {
+            console.log(response);
+            const weatherObj = response.json();
+            return weatherObj;
+        })
 
-    .then(function (weatherObj){
-        console.log(weatherObj)
-        console.log(weatherObj.coord.lat);
-    })
+        .then(function (weatherObj) {
+            console.log(weatherObj);
+            console.log(weatherObj.coord.lat);
+            weather.innerText = ` ${weatherObj.name}  ${weatherObj.main.temp} ℃`;
+        });
 }
-function success (position) {
+function success(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     getWeather(lat, lon);
-
 }
 function fail() {
     console.log('fail to get coords');
 }
-function getCoords () {
-    
-    navigator.geolocation.getCurrentPosition(success,fail)
+function getCoords() {
+    navigator.geolocation.getCurrentPosition(success, fail);
 }
 
-function loadWeather () {
+function loadWeather() {
     currentCoords = localStorage.getItem(COORDS);
 
     if (currentCoords === null) {
         getCoords();
     } else {
-
     }
-
-    
 }
-
-
 
 loadWeather();
