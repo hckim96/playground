e8d77372089b47569e253573dcd0f04b80016f3b
hyeoncheolkim <hyeoncheolkim96@gmail.com>
2020-07-03 23:58:12 +0900

    send msg from bg to content

--- chrome-extension/ct-extension/content.js
+++ chrome-extension/ct-extension/content.js
@@ -1,8 +1,14 @@
 console.log('chrome extension is ready to go!!!');
 
-let paragraphs = document.getElementsByTagName('p');
-for (e of paragraphs) {
-    e.style['background-color'] = 'red';
-}
-
 // content script does not run until the page is loaded (by the def of content script)
+
+chrome.runtime.onMessage.addListener(gotMessage);
+function gotMessage(request, sender, sendResponse) {
+    console.log(request.txt);
+    if (request.txt === 'hello') {
+        let paragraphs = document.getElementsByTagName('p');
+        for (e of paragraphs) {
+            e.style['background-color'] = 'red';
+        }
+    }
+}
