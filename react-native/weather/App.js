444e1bfbef3568e4dc4118aec72348820ff1522d
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-09 11:59:54 +0900

    d

--- react-native/weather/App.js
+++ react-native/weather/App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+// import axios from 'axios';
+// const API_KEY;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// const getWeather(latitude, longitude){
+
+// }
+const getLocation = async () => {
+    const result = await Location.getCurrentPositionAsync();
+    console.log(result.coords.latitude, result.coords.longitude);
+    return result.coords;
+};
+
+export default class App extends React.Component {
+    state = {
+        coords: { latitude: 1, longitude: 2 },
+    };
+    async componentDidMount() {
+        const coords = await getLocation();
+        console.log(coords);
+        this.setState({
+            coords: { latitude: coords.latitude, longitude: coords.longitude },
+        });
+    }
+
+    render() {
+        const { coords } = this.state;
+
+        return (
+            <View style={styles.container}>
+                <Text>
+                    latitude : {coords.latitude} longitude: {coords.longitude}
+                </Text>
+                <StatusBar style='auto' />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
