e4e17246c25adc93249e5e049057a94704eb7860
hckim96 <hyeoncheolkim96@gmail.com>
2020-07-13 14:24:28 +0900

    opts

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -4,6 +4,7 @@ import resolvers from './graphql/resolvers';
 const server = new GraphQLServer({
     typeDefs: './graphql/schema.graphql',
     resolvers: resolvers,
+    opts: { port: 7777, endpoint: '/graphql' },
 });
 server.start(() => {
     console.log('graphql server started');
